e58225a283fb8778ca132bb2c95be66ee108c90f
Yong Kang 永康 <100660214+vinhkhang20@users.noreply.github.com>
2022-07-14 08:53:51 +0700

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -3,6 +3,10 @@ import streamlit as st
 import numpy as np
 import cv2
 from  PIL import Image, ImageEnhance 
+import urllib.request
+
+urllib.request.urlretrieve(‘https://yt3.ggpht.com/ytc/AKedOLR5fxrufVjYQu2fTYqWR9EwxmJ6MiXFDGRvwohZ_A=s900-c-k-c0x00ffffff-no-rj’, “file_name”)
+image = Image.open(“file_name”)
 
 #Create two columns with different width
 col1, col2 = st.columns( [0.8, 0.2])
@@ -12,7 +16,8 @@ with col1:               # To display the header text using css style
     </style> """, unsafe_allow_html=True)
     st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
     
-
+with col2:               # To display brand logo
+    st.image(image,  width=300) 
 
 #Add a header and expander in side bar
 st.sidebar.markdown('<p class="font">Turn your photo to amazing anime shots appplication</p>', unsafe_allow_html=True)
@@ -30,35 +35,7 @@ if uploaded_file is not None:
     st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
     st.image(image, width= 500)  
     st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True) 
-
-# # Add conditional statements to take the user input values
-#     filter = st.sidebar.radio('Covert your photo to:', ['Original','Gray Image','Black and White', 'Pencil Sketch', 'Blur Effect'])
-#     if filter == 'Gray Image':
-#             converted_img = np.array(image.convert('RGB'))
-#             gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-#             st.image(gray_scale, width=300)
-#     elif filter == 'Black and White':
-#             converted_img = np.array(image.convert('RGB'))
-#             gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-#             slider = st.sidebar.slider('Adjust the intensity', 1, 255, 127, step=1)
-#             (thresh, blackAndWhiteImage) = cv2.threshold(gray_scale, slider, 255, cv2.THRESH_BINARY)
-#             st.image(blackAndWhiteImage, width=300)
-#     elif filter == 'Pencil Sketch':
-#             converted_img = np.array(image.convert('RGB')) 
-#             gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-#             inv_gray = 255 - gray_scale
-#             slider = st.sidebar.slider('Adjust the intensity', 25, 255, 125, step=2)
-#             blur_image = cv2.GaussianBlur(inv_gray, (slider,slider), 0, 0)
-#             sketch = cv2.divide(gray_scale, 255 - blur_image, scale=256)
-#             st.image(sketch, width=300) 
-#     elif filter == 'Blur Effect':
-#             converted_img = np.array(image.convert('RGB'))
-#             slider = st.sidebar.slider('Adjust the intensity', 5, 81, 33, step=2)
-#             converted_img = cv2.cvtColor(converted_img, cv2.COLOR_RGB2BGR)
-#             blur_image = cv2.GaussianBlur(converted_img, (slider,slider), 0, 0)
-#             st.image(blur_image, channels='BGR', width=300) 
-#     else: 
-#             st.image(image, width=300) 
+    
 
     #Turn the photo into anime pic
     converted_img = np.array(image.convert('RGB'))
